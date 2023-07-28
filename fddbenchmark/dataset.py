ca2d83c9d1962b14e2a87e05ca215d7e7d236d7a
Platon Ivanov <i1080p-ti@live.ru>
2023-07-28 22:03:41 +0300

    add lessmeier_bearing dataset,avoid code dublicate

--- fddbenchmark/dataset.py
+++ fddbenchmark/dataset.py
@@ -12,61 +12,25 @@ class FDDDataset():
         self.labels = None
         self.train_mask = None
         self.test_mask = None
-        available_datasets = ['small_tep', 'reinartz_tep', 'rieth_tep']
+        available_datasets = ['small_tep', 'reinartz_tep', 'rieth_tep', 'lessmeier_bearing']
         available_datasets_str = ', '.join(available_datasets)
         if self.name not in available_datasets:
             raise Exception(
                 f'{name} is an unknown dataset. Available datasets are: {available_datasets_str}'
             )
-        if self.name == 'small_tep':
-            self.load_small_tep()
-        if self.name == 'reinartz_tep':
-            self.load_reinartz_tep()
-        if self.name == 'rieth_tep':
-            self.load_rieth_tep()
+        if self.name in ['small_tep', 'reinartz_tep', 'rieth_tep']:
+            self.load_file_by_name(self.name)
+        elif self.name == "lessmeier_bearing":
+            self.load_file_by_name("lessmeier_bearing_4n1of20")
         
-    def load_small_tep(self):
-        ref_path = 'data/small_tep/'
+    def load_file_by_name(self, filename: str):
+        ref_path = 'data/' + filename + '/'
         if not os.path.exists(ref_path):
-            os.makedirs(ref_path)
-        url = "https://industrial-makarov.obs.ru-moscow-1.hc.sbercloud.ru/small_tep.zip"
-        zfile_path = 'data/small_tep.zip'
+            os.makedirs(ref_path)        
+        url = "https://industrial-makarov.obs.ru-moscow-1.hc.sbercloud.ru/" + filename + ".zip"
+        zfile_path = 'data/' + filename + '.zip'
         if not os.path.exists(zfile_path):
-            download_pgbar(url, zfile_path, fname='small_tep.zip')
-        
-        extracting_files(zfile_path, ref_path)
-        self.df = read_csv_pgbar(ref_path + 'dataset.csv', index_col=['run_id', 'sample'])
-        self.labels = read_csv_pgbar(ref_path + 'labels.csv', index_col=['run_id', 'sample'])['labels']
-        train_mask = read_csv_pgbar(ref_path + 'train_mask.csv', index_col=['run_id', 'sample'])['train_mask']
-        test_mask = read_csv_pgbar(ref_path + 'test_mask.csv', index_col=['run_id', 'sample'])['test_mask']
-        self.train_mask = train_mask.astype('boolean')
-        self.test_mask = test_mask.astype('boolean')
-    
-    def load_reinartz_tep(self):
-        ref_path = 'data/reinartz_tep/'
-        if not os.path.exists(ref_path):
-            os.makedirs(ref_path)
-        url = "https://industrial-makarov.obs.ru-moscow-1.hc.sbercloud.ru/reinartz_tep.zip"
-        zfile_path = 'data/reinartz_tep.zip'
-        if not os.path.exists(zfile_path):
-            download_pgbar(url, zfile_path, fname='reinartz_tep.zip')
-        
-        extracting_files(zfile_path, ref_path)
-        self.df = read_csv_pgbar(ref_path + 'dataset.csv', index_col=['run_id', 'sample'])
-        self.labels = read_csv_pgbar(ref_path + 'labels.csv', index_col=['run_id', 'sample'])['labels']
-        train_mask = read_csv_pgbar(ref_path + 'train_mask.csv', index_col=['run_id', 'sample'])['train_mask']
-        test_mask = read_csv_pgbar(ref_path + 'test_mask.csv', index_col=['run_id', 'sample'])['test_mask']
-        self.train_mask = train_mask.astype('boolean')
-        self.test_mask = test_mask.astype('boolean')
-
-    def load_rieth_tep(self):
-        ref_path = 'data/rieth_tep/'
-        if not os.path.exists(ref_path):
-            os.makedirs(ref_path)
-        url = "https://industrial-makarov.obs.ru-moscow-1.hc.sbercloud.ru/rieth_tep.zip"
-        zfile_path = 'data/rieth_tep.zip'
-        if not os.path.exists(zfile_path):
-            download_pgbar(url, zfile_path, fname='rieth_tep.zip')
+            download_pgbar(url, zfile_path, fname=filename+'.zip')
         
         extracting_files(zfile_path, ref_path)
         self.df = read_csv_pgbar(ref_path + 'dataset.csv', index_col=['run_id', 'sample'])
