28a89d2612e0299a667f9ba57c151dace9bd766f
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2023-10-17 13:18:41 +0300

    Update __init__.py

--- fddbenchmark/__init__.py
+++ fddbenchmark/__init__.py
@@ -5,5 +5,15 @@ Benchmarking fault detection and diagnosis methods.
 """
 
 __version__ = "0.0.1"
-__author__ = 'Vitaliy Pozdnyakov'
+__author__ = 'Vitaliy Pozdnyakov, Mikhail Goncharov, Platon Ivanov'
 __credits__ = 'AIRI'
+
+from .dataset import FDDDataset
+from .dataloader import FDDDataloader
+from .evaluator import  FDDEvaluator
+
+__all__ = [
+    'FDDDataset',
+    'FDDDataloader',
+    'FDDEvaluator',
+]
